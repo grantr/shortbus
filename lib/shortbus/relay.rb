c7ce3d783e47434c0b2f296cb12308b935fbabcf
Grant Rodgers <grantr@gmail.com>
2012-09-22 10:15:43 -0700

    add a new group for the relay
    The relay doesn't care about snapshots, it is only for pubsub.

---
+++ lib/shortbus/relay.rb
@@ -0,0 +1,17 @@
+require 'celluloid'
+require 'shortbus/collector'
+require 'shortbus/sequencer'
+require 'shortbus/digester'
+require 'shortbus/pusher'
+require 'shortbus/file_persister'
+require 'shortbus/snapshotter'
+
+module Shortbus
+  class Group < Celluloid::SupervisionGroup
+    supervise Sequencer, as: :sequencer
+    supervise Digester, as: :digester
+    #supervise Pusher, as: :pusher, args: ['0.0.0.0', 3001]
+    supervise FilePersister, as: :persister
+    #supervise Snapshotter, as: :snapshotter
+  end
+end
