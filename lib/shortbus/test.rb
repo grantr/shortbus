a8d9a754a6637c82311a18739d6c096711cf0a9a
Grant Rodgers <grantr@gmail.com>
2012-09-22 10:17:33 -0700

    test with the relay

--- lib/shortbus/test.rb
+++ lib/shortbus/test.rb
@@ -1,5 +1,4 @@
-require 'shortbus/group'
-require 'shortbus/collector'
+require 'shortbus/relay'
 b = Shortbus::Collector.new("0.0.0.0", 3000)
 p = Shortbus::Pusher.supervise("0.0.0.0", 3010)
 Shortbus::Group.run!
